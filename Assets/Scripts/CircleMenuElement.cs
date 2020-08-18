0de6454fb1cd8453c976d9d8f9fd22e750e85be0
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-08-18 21:18:33 +0530

    Added Camera Controller and Wall Builder

---
+++ Assets/Scripts/CircleMenuElement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class CircleMenuElement : MonoBehaviour
+{
+    public string Name;
+    public string Description;
+    public Image buttonBackground;
+    public Sprite buttonIcon;
+}
---
+++ Assets/Scripts/RadialUI.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RadialUI : MonoBehaviour
+{
+    public Camera UICamera;
+    public GameObject backgroundPanel;
+    public GameObject circleMenuElement;
+    public bool gradient;
+
+    public Color normalColor;
+    public Color highlightColor;
+    public Gradient highlightColorGradient = new Gradient();
+
+    public Image centerBackground;
+    public Text itemName;
+    public Text itemDescription;
+    public Image itemIcon;
+
+    private int currentMenuItem;
+    private int previousMenuItem;
+    private float calculateMenuIndex;
+    private float currentAngle;
+    private Vector3 currentMousePosition;
+    private List<CircleMenuElement> menuElements = new List<CircleMenuElement>();
+
+    private static RadialUI instance;
+
+    public static RadialUI Instance
+    {
+        get { return instance; }
+    }
+    public bool Active
+    {
+        get { return backgroundPanel.activeSelf; }
+    }
+
+    public List<CircleMenuElement> MenuElements
+    {
+        get { return menuElements; }
+        set { menuElements = value; }
+    }
+
+    public void Initialize()
+    {
+        float rotationalIncrement = 360f / menuElements.Count;
+        float currentRotationalValue = 0;
+        float fillPercentageValue = 1f / menuElements.Count;
+        for (int i = 0; i < menuElements.Count; i++)
+        {
+            GameObject menuElementGameObject = Instantiate(circleMenuElement);
+            menuElementGameObject.name = i + ":" + currentRotationalValue;
+            menuElementGameObject.transform.SetParent(backgroundPanel.transform);
+
+            MenuButton menuButton = menuElementGameObject.GetComponent<MenuButton>();
+
+            menuButton.rectTransform.localScale = Vector3.one;
+            menuButton.rectTransform.localPosition = Vector3.zero;
+            menuButton.rectTransform.rotation= Quaternion.Euler(0,0,currentRotationalValue);
+        }
+    }
+}
