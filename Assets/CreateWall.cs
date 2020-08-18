7547f758735228dac0d21a90b0f58f7178b5b4ee
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-08-18 21:59:42 +0530

    Added Diagonal Walls

--- Assets/CreateWall.cs
+++ Assets/CreateWall.cs
@@ -11,6 +11,12 @@ public class CreateWall : MonoBehaviour
     public GameObject wall;
     GameObject lastpole;
     cameraController _cameraController;
+
+    int WallIndex;
+    GameObject wallParent;
+    public GameObject Walls;
+    public int money = 100;
+    public int cost = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,17 +34,31 @@ public class CreateWall : MonoBehaviour
     }
     void getInput()
     {
-        if (Input.GetMouseButtonDown(0)) { startBuilding(); }
-        else if (Input.GetMouseButtonUp(0)) { endBuilding(); }
-        else { if (isCreating) { updateWall(); } }
+        if (money >= cost)
+        {
+            if (Input.GetMouseButtonDown(0)) { startBuilding(); }
+            else if (Input.GetMouseButtonUp(0)) { endBuilding(); }
+            else { if (isCreating) { updateWall(); } }
+        }
+        else
+        {
+            isCreating = false;
+
+            _cameraController.enabled = true;
+
+        }
     }
 
     void startBuilding()
     {
         isCreating = true;
         _cameraController.enabled = false;
+        WallIndex++;
+        wallParent = new GameObject();
+        wallParent.transform.parent = Walls.transform;
+        wallParent.name = "WallParent" + WallIndex.ToString();
         Vector3 startPos = buildPosition.getSnappedPoint(buildPosition.getWorldPoint());
-        GameObject startPole = Instantiate(pole, startPos, Quaternion.identity);
+        GameObject startPole = Instantiate(pole, startPos, Quaternion.identity, wallParent.transform);
         startPole.transform.position = new Vector3(startPos.x, startPos.y + 0.5f, startPos.z);
         lastpole = startPole;
     }
@@ -58,10 +78,13 @@ public class CreateWall : MonoBehaviour
     }
     void createWallSegement(Vector3 current)
     {
-        GameObject newPole = Instantiate(pole, current, Quaternion.identity);
+        GameObject newPole = Instantiate(pole, current, Quaternion.identity, wallParent.transform);
         Vector3 mid = Vector3.Lerp(newPole.transform.position, lastpole.transform.position, 0.5f);
-        GameObject newWall = Instantiate(wall, mid, Quaternion.identity);
+        GameObject newWall = Instantiate(wall, mid, Quaternion.identity, wallParent.transform);
+        money -= cost;
         newWall.transform.LookAt(lastpole.transform);
+        float dist = Vector3.Distance(newPole.transform.position, lastpole.transform.position);
+        newWall.transform.localScale = new Vector3(newWall.transform.localScale.x, newWall.transform.localScale.y, dist);
         lastpole = newPole;
     }
 }
