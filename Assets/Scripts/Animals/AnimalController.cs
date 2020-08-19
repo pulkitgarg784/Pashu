c3ba3a7762781f2bdeabe857a60e7baf6f459894
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-08-19 22:38:40 +0530

    Added basic locomotion in Animals with animations

---
+++ Assets/Scripts/Animals/AnimalController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AnimalController : MonoBehaviour
+{
+    public Camera cam;
+    public NavMeshAgent agent;
+    public Animator animator;
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+
+        if (agent.remainingDistance > agent.stoppingDistance + 3f)
+        {
+            animator.SetBool("isRunning", true);
+            agent.speed = Mathf.Lerp(agent.speed, 3, .1f);
+        }
+        else if (agent.remainingDistance > agent.stoppingDistance + 0.1f)
+        {
+            animator.SetBool("isRunning", false);
+            animator.SetBool("isWalking", true);
+            agent.speed = Mathf.Lerp(agent.speed, 1, .1f);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+
+        }
+    }
+}
