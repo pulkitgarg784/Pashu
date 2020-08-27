8825a202e26e1ec23e5d597b15fb0135bb3e5ce8
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-08-27 13:27:53 +0530

    Added month end bills

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using System.Globalization;
+using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
     private DateTime theDate;
+    private Stats stats;
     public int dayOfYear = 1;
+    public GameObject costPanel;
+    public Text costtext;
+    int bill;
+    bool stopTime;
     void Start()
     {
         convertDaytoDate();
         InvokeRepeating("NextDay", 30, 30);
+        stats = FindObjectOfType<Stats>();
+
     }
 
     // Update is called once per frame
@@ -29,13 +37,23 @@ public class TimeManager : MonoBehaviour
     }
     void NextDay()
     {
-        if (theDate.Day == DateTime.DaysInMonth(theDate.Year, theDate.Month))
+        if (!stopTime)
         {
-            Debug.Log("Month End");
-            Time.timeScale = 0;
+            if (theDate.Day == DateTime.DaysInMonth(theDate.Year, theDate.Month))
+            {
+                Debug.Log("Month End");
+                costPanel.SetActive(true);
+                stopTime = true;
+                int govt = stats.animalCount * 100;
+                int donations = UnityEngine.Random.Range(100, 500);
+                int rent = UnityEngine.Random.Range(1000, 2000);
+                bill = govt + donations - rent;
+                costtext.text = "Govt. funds : $" + govt.ToString() + Environment.NewLine + "Donations: $" + donations.ToString() + Environment.NewLine + "Rent and costs: $" + rent.ToString() + Environment.NewLine + "Total: $" + bill.ToString();
+                Time.timeScale = 0.01f;
+            }
+            dayOfYear++;
+            convertDaytoDate();
         }
-        dayOfYear++;
-        convertDaytoDate();
 
     }
     void convertDaytoDate()
@@ -44,5 +62,10 @@ public class TimeManager : MonoBehaviour
         int year = DateTime.Now.Year; //Or any year you want
         theDate = new DateTime(year, 1, 1).AddDays(dayOfYear - 1);
         UIManager.instance.setDayMonth(theDate.Day.ToString(), CultureInfo.CurrentCulture.DateTimeFormat.GetAbbreviatedMonthName(theDate.Month).ToUpper());
+    }
+    public void PayBills()
+    {
+        stats.Money += bill;
+        UIManager.instance.UpdateValues();
     }
 }
