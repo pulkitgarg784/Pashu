77477f72df3f1a439d168d169cfa977bb0d3fa15
pulkitgarg784 <33174851+pulkitgarg784@users.noreply.github.com>
2020-08-30 00:16:15 +0530

    Added ingame tutorial script

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public GameObject[] panels;
+    private int panelIndex;
+
+
+    private void Start()
+    {
+        panelIndex = 0;
+    }
+    void Update()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            if (i == panelIndex)
+            {
+                panels[i].SetActive(true);
+            }
+            else
+            {
+                panels[i].SetActive(false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            panelIndex++;
+        }
+        if (panelIndex >= panels.Length)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
